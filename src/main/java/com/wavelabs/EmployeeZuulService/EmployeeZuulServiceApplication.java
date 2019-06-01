10e18c1c96d99988bba9ab344d6dbea9bc5a6493
kiran.kumar <kiran.kumar@care.com>
2019-06-01 09:20:05 +0530

    initial commit

---
+++ src/main/java/com/wavelabs/EmployeeZuulService/EmployeeZuulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wavelabs.EmployeeZuulService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeZuulServiceApplication.class, args);
+	}
+
+}
